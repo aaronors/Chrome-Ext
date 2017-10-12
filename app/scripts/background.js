254a65eb11df550b32a6188d67471354534460a7
aaronors <pinoypinay93>
2017-10-11 17:03:55 -0700

    started on detecting vid

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -7,3 +7,10 @@ chrome.runtime.onInstalled.addListener(details => {
 chrome.browserAction.setBadgeText({text: '\'Allo'});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+	// for the current tab, inject the "inject.js" file & execute it
+	chrome.tabs.executeScript(tab.ib, {
+		file: 'inject.js'
+	});
+});
---
+++ app/scripts/inject.js
@@ -0,0 +1,8 @@
+(function() {
+
+
+  document.getElementsByTagName('video')[0].pause();
+
+	alert('inserted self... giggity');
+
+})();
