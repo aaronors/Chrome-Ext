02419d5681493cb01229ebd5deb37d434cf420bc
aaronors <pinoypinay93>
2017-10-18 16:30:16 -0700

    more placeholder logic

--- app/scripts/controllers/list.controller.js
+++ app/scripts/controllers/list.controller.js
@@ -24,47 +24,75 @@ angular.module('my-extension')
 		}];
 	
 		$scope.tempholder = [ "Place link here" ];
+	  $scope.dragDirection = '';
+    $scope.hovered = false;
 	
 	$scope.sortableOptions = {
 		connectWith: ".drop-target",
-		placeholder: ".list-item"
-
+		tolerance: 'pointer',
+		start: onDragStart,
+    stop: onDragStop,
+    over: onDragOver,
+    out: onDragOut
 	}
 	
 	$scope.$watch(function(scope) {return scope.nowplaying.length},
 							function(newValue,oldValue){
-								if(newValue != oldValue){
+								if(newValue > 1){
 							 		//alert('list has changed'+ newValue);
 								
 								}
 							});
 	
-	$scope.isEmpty = function(){
-		if (!$scope.nowplaying) { // when the last item is removed, this is called without a model.
-      return true;
-     } 
-		else if($scope.nowplaying.length === 0){
-			
-			return true;
-		}
-		else{
-			
-			return false; 
-		}
-	}
 	
 	$scope.addCurrentTab = function(){
 		chrome.tabs.query({currentWindow: true, active: true},function(tabs) { // null defaults to current window
-  	var tabtitle = tabs[0].title;
-		var taburl = tabs[0].url; 
-		$scope.items.unshift({
-			title: tabtitle,
-			url: taburl
+			var tabtitle = tabs[0].title;
+			var taburl = tabs[0].url; 
+			$scope.items.unshift({
+				title: tabtitle,
+				url: taburl
+			});
+		// ...
 		});
-  // ...
-});
 	}
 	
+        function onDragStart() {
+          // If the drag starts here, then we are dragging out.
+          $scope.$apply(function() {
+            $scope.dragDirection = 'drag-out';
+          });
+        }
+
+        function onDragStop() {
+          $scope.$apply(function() {
+            $scope.dragDirection = '';
+          });
+        }
+
+        function onDragOver() {
+          $scope.$apply(function() {
+            $scope.hovered = true;
+          });
+        }
+
+        function onDragOut() {
+          $scope.$apply(function() {
+            $scope.hovered = false;
+          });
+        }
+
+        $scope.isEmpty = function() {
+          if (!$scope.nowplaying) { // when the last item is removed, this is called without a model.
+            return true;
+          } else if ($scope.dragDirection === 'drag-out' && !$scope.hovered) { // when dragging last item out of a list
+            return $scope.nowplaying.length <= 1;
+          } else if ($scope.hovered) {
+            return false;
+          } else {
+            return $scope.nowplaying.length === 0;
+          }
+        };
 });
 
 
